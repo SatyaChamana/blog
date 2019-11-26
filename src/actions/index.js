f5d092246000afacf6962065f48f2a23665eee9b
Satya Chamana <satyachamana@gmail.com>
2019-11-26 11:47:53 +0530

    final Commit for blog app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,16 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUser = () =>  async (dispatch,getState) => {
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
+
+};
 
 export const fetchPosts = () =>  async dispatch => {
     const response = await jsonPlaceholder.get('./posts');
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -5,9 +5,7 @@ import {fetchUser} from '../actions';
 
 class UserHeader extends React.Component{
 
-    componentDidMount = () => {
-        this.props.fetchUser(this.props.userId)
-    }
+ 
     render () {
         const user = this.props.userName
         if (!user){
